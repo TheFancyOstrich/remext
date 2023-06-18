e2cb3b19158c42f6c7cb67b5cf18e75430cea186
Erik <erik.engstedt@proton.me>
2023-06-18 09:36:18 +0200

    Fix user path error

--- config/config.exs
+++ config/config.exs
@@ -2,5 +2,5 @@ import Config
 version = "0.1.0"
 
 if config_env() == :release do
-  config :remext, filepath: "~/.config/remext.json", version: version
+  config :remext, filepath: System.user_home!() <> "/.config/remext.json", version: version
 end
