1aa1b9969683a31ac3dcb1d7e64406c4d333bff3
Erik <erik.engstedt@proton.me>
2023-06-25 13:59:52 +0200

    Clean repo

--- config/config.exs
+++
@@ -1,5 +0,0 @@
-import Config
-
-if config_env() == :release do
-  config :remext, filepath: System.user_home!() <> "/.config/remext.json"
-end
--- test/remext_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule RemextTest do
-  use ExUnit.Case
-  doctest Remext
-
-  test "greets the world" do
-    assert Remext.hello() == :world
-  end
-end
